d6e8c679c311de68445d3cde1d39268889a4c57f
Shenon69 <ask.tikki69@gmail.com>
2023-05-14 22:33:20 +0530

    trishans test

--- Hostel_Management_System/Form_Login.cs
+++ Hostel_Management_System/Form_Login.cs
@@ -76,7 +76,8 @@ namespace Hostel_Management_System
 
         private void btn_login_Click(object sender, EventArgs e)
         {
-
+            test objtest = new test();
+            objtest.Show();
         }
 
         private void form_login_Load(object sender, EventArgs e)
