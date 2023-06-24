a02b7ad7573af5414ff370d1c679fc7b36791955
WasathTheekshana <wasath.vt@gmail.com>
2023-06-24 18:29:31 +0530

    feat: add login functionality to the interface and set the user state

---
+++ Hostel_Management_System/Logged In Users/User_State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel_Management_System.Logged_In_Users
+{
+    internal class User_State
+    {
+        private string userName; 
+
+        public void setUserName(string userName)
+        {
+            this.userName = userName;
+        }
+
+        public string getUserName()
+        {
+            return userName;
+        }
+    }
+}
