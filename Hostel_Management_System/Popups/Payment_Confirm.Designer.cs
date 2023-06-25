994d512c1ac49f09eedf5232aace0d10dc773b07
WasathTheekshana <wasath.vt@gmail.com>
2023-06-26 00:44:40 +0530

    fghdhdgdgdfgdg

--- Hostel_Management_System/Popups/Payment_Confirm.Designer.cs
+++ Hostel_Management_System/Popups/Payment_Confirm.Designer.cs
@@ -29,11 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.SuspendLayout();
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.TargetControl = this;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel1.Location = new System.Drawing.Point(395, 184);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(68, 15);
+            this.gunaLabel1.TabIndex = 0;
+            this.gunaLabel1.Text = "gunaLabel1";
+            // 
+            // Payment_Confirm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gunaLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Payment_Confirm";
             this.Text = "Payment_Confirm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
     }
 }
